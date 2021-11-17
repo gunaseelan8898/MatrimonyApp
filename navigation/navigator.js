69dfd82a76fd83d3381acb401e775f3d47289f02
gunaseelan8898 <gunaseelanravi98r@email.com>
2021-11-17 12:12:34 +0000

    Signup & HomeScreen

--- navigation/navigator.js
+++ navigation/navigator.js
@@ -3,10 +3,29 @@ import { createAppContainer } from "react-navigation";
 
 import splashScreen from "../screens/splashScreen";
 import SignInScreen from "../screens/signInScreen";
+import SignUpScreen from "../screens/signUpScreen";
+import homeScreen from "../screens/homeScreen";
+
+const AuthNavigator = createStackNavigator({
+    Splash:splashScreen,
+    SignIn:SignInScreen,
+    SignUp:SignUpScreen,
+},
+{
+    headerMode:'none'
+})
+
+const HomeNavigator = createStackNavigator({
+     home:homeScreen,
+},
+{
+    headerMode:'none'
+}
+    )
 
 const Root = createStackNavigator({
-    Splash:splashScreen,
-    SignIn:SignInScreen
+    Auth:AuthNavigator,
+    Home:HomeNavigator
 },
 {
     headerMode:'none'
